0623ad091440b44b62bcbfeb0dca4989b20d60a3
Ben Ahlbrand <bmahlbrand@gmail.com>
2020-09-13 04:41:53 -0400

    feat: add carousel

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -18,6 +18,14 @@
 
 
     <div class="max-w-screen-xl mx-auto flex flex-wrap mb-20">
+<div>
+    <VueSlickCarousel :arrows="true" :dots="true">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </VueSlickCarousel>
+  </div>
 
       <div class="flex w-full md:w-1/2 ">
         <a href="https://frl.nyu.edu">
@@ -78,10 +86,15 @@ query {
 
 <script>
 import HeroHeader from "~/components/HeroHeader.vue";
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
   components: {
     HeroHeader,
+    VueSlickCarousel
   },
   data: () => ({}),
   metaInfo: {
